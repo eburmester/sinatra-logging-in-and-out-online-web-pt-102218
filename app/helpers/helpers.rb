272ce074860d6e60038625f50fe6a53cd660f2fb
Elyse Burmester <burmesterelyse@gmail.com>
2019-03-13 20:56:37 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   
-  def self.current_user(session)
-    User.find(session[:user_id])
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
   end
-  
-  def self.logged_in?(session)
-    !!session[:user_id]
+
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
   end
   
 end
